889d499fb48020fc68f06ea9cd2193c24a6c3d6a
erika e jeniffer <erikasena.avila@gmail.com>
2024-03-19 12:50:24 -0300

    feat: Finalizado layout

---
+++ TipCalculator/MainPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿namespace TipCalculator
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        public void Button15Percent(object sender, EventArgs e)
+        {
+            TipPercentLabel.Text = $" {PercentageSlider.Value.ToString()}";
+        }
+        public void Button30Percent(object sender, EventArgs e)
+        {
+
+        }
+        public void ButtonRoundUp(object sender, EventArgs e)
+        {
+
+        }
+        public void ButtonRoundDown(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+}
